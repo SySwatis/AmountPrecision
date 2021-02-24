4c2054b2df6975a5ca76f1d53f57e3b84ea6d665
syswatis <syswatis@gmail.com>
2021-02-24 11:21:34 +0100

    Initial commit

---
+++ registration.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ */
+
+/**
+ * @category   Syswatis
+ * @package    Syswatis_AmountPrecision
+ * @author SySwatis (Stéphane JIMENEZ)
+ * @copyright Copyright (c) 2020 SySwatis (http://www.syswatis.com)
+ */
+ 
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Syswatis_AmountPrecision',
+    __DIR__
+);
